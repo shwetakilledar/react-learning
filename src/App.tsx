5a1e8e445ea04f929a9073b6619f712d711310e4
Shweta <24240467+shwetakilledar@users.noreply.github.com>
2023-09-03 21:22:34 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { ExampleOne } from "./ExampleOne";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ExampleOne />
+    </div>
+  );
+}
---
+++ src/ExampleOne.tsx
@@ -0,0 +1,1 @@
+export const ExampleOne = () => <>EXAMPLE One</>;
